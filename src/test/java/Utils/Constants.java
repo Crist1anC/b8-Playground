660c5f7872bc7b495a2045bd092a34fa17eb7aa5
Christ1anC <cristian008c@gmail.com>
2021-07-11 18:36:29 -0500

    Added the Constants for baseURI and basePath

---
+++ src/test/java/Utils/Constants.java
@@ -0,0 +1,11 @@
+package Utils;
+
+public class Constants {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String ACCEPT = "Accept";
+    public static final String CONTENT_TYPE = "Content-Type";
+
+
+
+}
